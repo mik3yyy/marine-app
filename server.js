df0b1fbc3a4163aa35a5f3f19876be1da23edb99
Okpechi Chibuikem Michael <mikeokpechi@gmail.com>
2022-09-05 15:40:51 +0100

    1.0.3

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const mongoose =require('mongoose');
 
 mongoose.connect("mongodb+srv://chinedum:CHInedum12@cluster0.unoopz3.mongodb.net/?retryWrites=true&w=majority").then((e)=>console.log('database connected'));
 
-app.listen(3000, (req,res)=>console.log('looged in '));
+app.listen(process.env.PORT||3000, (req,res)=>console.log('looged in '));
 
 
