c524d9f8cba5ea3807e0bee3d9cf51c144ff81ec
Okpechi Chibuikem Michael <mikeokpechi@gmail.com>
2022-09-07 12:21:42 +0100

    1.1.3

--- routes/tranferRoute.js
+++ routes/tranferRoute.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const transferRoute=express.Router();
 const transferController= require('../controllers/transferController')
-transferRoute.post('/transfer', transferController.Transefer);
+transferRoute.post('/transfer', transferController.Transfer);
 transferRoute.post('/transfer/findaccount',transferController.findAccount);
 
 module.exports=transferRoute;
