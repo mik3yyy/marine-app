de492fd6b691f9581ae51404b089bc4c2a33ba86
mik3yyy <mikeokpechi@gmail.com>
2023-11-30 22:54:22 +0100

    new build

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const app = require('./app');
 const mongoose =require('mongoose');
 
-mongoose.connect("mongodb+srv://Chinedum:CHInedum12@cluster0.ldpmg7r.mongodb.net/virtual_classroom").then((e)=>console.log('database connected'));
+const mongo_uri = "mongodb+srv://Michael:CHInedum12@cluster0.rl2r1sh.mongodb.net/?retryWrites=true&w=majority"
 
+
+mongoose.connect(mongo_uri).then((e)=>console.log('database connected'));
+//mongodb+srv://Chinedum:CHInedum12@cluster0.ldpmg7r.mongodb.net/virtual_classroom
 // mongodb+srv://chinedum:CHInedum12@cluster0.unoopz3.mongodb.net/?retryWrites=true&w=majority
 app.listen(process.env.PORT||8000, (req,res)=>console.log('looged in '));
 
